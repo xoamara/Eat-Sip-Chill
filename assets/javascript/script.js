25c99c7e9b282089947479a368d00eeb247ce024
SNAFU <nholdsworth@me.com>
2018-11-26 17:58:29 -0800

    added more stuff to the wine return still working on tracking down the issue with the recipe not returning

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,66 +13,70 @@ $(document).ready(function () {
     firebase.initializeApp(config);
 
     //MovieDB API Ajax calls
-
     let api_key = "81e30798ba964b88d42fd6064efd7734"
-    
+
     //Ajax call for movies based on Popularity
     $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page1"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
     })
 
     //Ajax call for Movies based on highest Ratings
     $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&language=en-US&certification_country=US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
     })
 
     //Ajax call for Movies based on highest Ratings for category "Drama"
     $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&with_genres=18&language=en-US&certification_country=US&certification=R&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
     })
 
     //Ajax call for Movies based on highest Ratings for category "Action"
     $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&with_genres=28&language=en-US&certification_country=US&certification=R&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
     })
 
     //Ajax call for Movies based on highest Ratings for category "Comedy"
     $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&with_genres=35&language=en-US&certification_country=US&certification=R&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
     })
 
     //Ajax call for Movies based on highest Ratings for category "Romance"
     $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&with_genres=10749&language=en-US&certification_country=US&certification=R&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
     })
 
     //Ajax call for Movies based on highest Ratings for category "Science Fiction"
     $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&with_genres=878&language=en-US&certification_country=US&certification=R&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
     })
 
 
     // On click function for use to submit their recipe request based on the ingredients in their fridge.
     $(document).on("click", "#find-recipes", function (event) {
+
         // prevent the page from refreshing
         event.preventDefault();
+
         // storing the user input in whatsInTheFridge variable so that it can be concatenated into the spoonacular search endpoint URL
         let whatsInTheFridge = $("#ingredient-input").val().trim();
         console.log(whatsInTheFridge);
+
+        // FIXME:push whatsInTheFridge to firebase so that it is always available as a variable for the URL's
+        // database.ref().push()
 
         // creating a vairable to store the relevant URL for the user search
         let whatsInTheFridgeURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + whatsInTheFridge + "&limitLicense=false&number=5&ranking=1";
@@ -132,43 +136,17 @@ $(document).ready(function () {
     // When the user clicks on a specific recipe (."shadow"), run the code below
     $(document).on("click", ".shadow", function (event) {
 
-        // storing the user input in whatsInTheFridge variable so that it can be concatenated into the spoonacular search endpoint URL
-        let whatsInTheFridge = $("#ingredient-input").val().trim();
-        console.log(whatsInTheFridge);
+        // resetting the values so that it only shows for most recently clicked recipe 
+        $("#wine-pairing").val("");
+        $("instructions").val("");
 
-        // TODO: still working on returning wine pairing information but there is an object in the console
-        let winePairingURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + whatsInTheFridge + "&maxPrice=15";
-
-        // Clear value of search input 
-        // FIXME: Nate moved this so that he could use the whatsInTheFridge variable again for the second on click event so that he could get the wine pairing returned based on the user food input 
-        $("#ingredient-input").val("");
-
-        // ajax call for wine pairing info
-        $.ajax({
-            beforeSend: function (request) {
-                request.setRequestHeader("X-Mashape-Key", "Opn7fgzDzImshjKftqensUuVw0XIp1SkMBQjsnfyMPrMiQydkc");
-            },
-
-            dataType: "Json",
-            method: "GET",
-            url: winePairingURL
-
-        // Once the promise is returned run the code below
-        }).then(function (response) {
-
-            // variable holding the ajax data object for the wine pairing
-            let results = response;
-            console.log(results);
-
-            let winePairing = $("<div>").text(results.pairingText);
-            $("#wine-pairing").append(winePairing);
-
-        });
+        // prevent the page from refreshing
+        event.preventDefault();
 
         // find the value attribute of $(this) recipeImage and hold it in the uniqueRecipeId variable so that it can be dynamically inserted into the queryURL of the  unique recipe information api call.  
         let uniqueRecipeId = $(this).attr("value");
 
-        // query Url variable for unique recipe api call
+        // query Url variable for unique recipe api call 
         let uniqueRecipeURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + uniqueRecipeId + "/information";
 
         // ajax call for unique recipe info
@@ -192,22 +170,86 @@ $(document).ready(function () {
             $("#food").hide()
 
             // dynamically creating a new div with the recipe instructions inside of it
-            let recipeInstructions = $("<div>").text(results.instructions);
+            let recipeInstructions = $("<p>").html(results.instructions);
+
+            // appending the recipeInstructions <div> to the <div> with id = "instrucitons"
+            $("#instructions").html(recipeInstructions);
+
+        });
+
+        // Bringing this variable down from the previous on click event to be used again here (scoping issue)
+        let whatsInTheFridge = $("#ingredient-input").val().trim();
+        console.log(whatsInTheFridge);
+
+        // TODO: still working on returning wine pairing information but there is an object in the console
+        let winePairingURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + whatsInTheFridge + "&maxPrice=15";
+
+        // Clear value of search input 
+        // FIXME: Nate moved this so that he could use the whatsInTheFridge variable again for the second on click event so that he could get the wine pairing returned based on the user food input 
+        // $("#ingredient-input").val("");
+
+        // ajax call for wine pairing info
+        $.ajax({
+            beforeSend: function (request) {
+                request.setRequestHeader("X-Mashape-Key", "Opn7fgzDzImshjKftqensUuVw0XIp1SkMBQjsnfyMPrMiQydkc");
+            },
+
+            dataType: "Json",
+            method: "GET",
+            url: winePairingURL
+            //FIXME: this is just for testing I am not really sure how to do an ajax call error yet
+            // error: alert("Dude Just go pick up 2 forties of Mad Dog 2020");
+
+            // Once the promise is returned run the code below
+        }).then(function (response) {
+
+            // variable holding the ajax data object for the wine pairing
+            let results = response;
+            console.log(results);
+
+            // creating divs to hold the specific wine data suggested from spoonacular
+            let pairingNotes = $("<p>").html(results.pairingText);
+            
+            // create a html element to hold the name of a specifically suggested wine
+            let specificWineSuggestion = $("<p>").html(results.productMatches[0].title);
+
+            // create a html element to hold that specific wine's description
+            let specificWineDescription = $("<p>").html(results.productMatches[0].description);
+
+            // appending those results to the div with id="wine-pairing"
+            $("#wine-pairing").append(pairingNotes);
+            $("#wine-pairing").append(specificWineSuggestion);
+            $("#wine-pairing").append(specificWineDescription);
 
             // dynamically creating a back button so that the user can get back to the recipes once they are reading instructions
             let backButton = $("<button>").attr("id", "back-button").addClass("button").text("BACK-BUTTON");
 
             // appending the recipeInstructions <div> to the <div> with id = "instrucitons"
-            $("#instructions").append(recipeInstructions);
-
-            // appending the recipeInstructions <div> to the <div> with id = "instrucitons"
-            $("#instructions").append(backButton);
+            $("#button2").html(backButton);
 
             // This on click event listener takes the user back to the recipes page after they are done looking at the cooking instructions 
             $(document).on("click", "#back-button", function (event) {
+
+                // prevent the page from refreshing
+                event.preventDefault();
+                // checking to see if the click event is getting registered
                 console.log("button click")
+
                 // hides the recipe instructions
                 $("#instructions").hide();
+
+                // Hide the back-button
+                $("#back-button").hide();
+
+                // hide the winepairing div
+                $("wine-pairing").hide();
+
+                // Erasing the text inside the wine-pairing div
+                $("#wine-pairing").text("");
+                
+                // Erasing the text inside the instructions div
+                $("instructions").text("");
+
                 // show the div with id = "food" 
                 $("#food").show();
 
